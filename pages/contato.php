29b9a83cbce80b1d118ab6d516304e1d71bac7b2
Larissa <larissavelsa@gmail.com>
2021-10-29 08:12:34 -0300

    alterando o layout do site ti.vgsul.sp.gov.br

---
+++ pages/contato.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <link rel="stylesheet" type="text/css" href="../static/css/style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <meta name="viewport" content="width=device-width, charset=utf-8 initial-scale=1" http-equiv="Content-Type">
+  </head>
+  <body>
+    <?php
+      include "nav.php";
+    ?>
+    
+
+		<div class="container">
+			<h3>Contato</h3>
+			<div class="panel panel-default">
+			<!--<div class="panel-heading">Contato</div>-->
+				<div class="panel-body">
+				<p><span class="glyphicon glyphicon-envelope"></span> prodav@vgsul.sp.gov.br</p>
+				<p><span class="glyphicon glyphicon-earphone"></span> (19) 3641-9039 | (19) 3641-9000</p>
+				<p><span class="glyphicon glyphicon-home"></span> Praça Washington Luiz, nº 643 - Centro - Vargem Grande do Sul-SP, CEP 13.880-000</p>
+				</div>
+		  </div>
+		</div>
+		
+		<div class="container">
+			<h3>Membros da equipe de TI</h3>
+			<div class="panel panel-default">
+			<!--<div class="panel-heading">Contato</div>-->
+				<div class="panel-body">
+				<p>Ricardo Vagner de Lima</p>
+				<p>Júlio César Morales</p>
+				<p>Larissa Velozo Sabiá</p>
+				<p>Willian César da Silva</p>
+				</div>
+		  </div>
+		</div>	
+
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
---
+++ pages/nav.php
@@ -0,0 +1,78 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet">
+
+<div class="container-fluid">
+<div class="container-centro">
+  <p><img src="../static/img/brasao-sem-fundo.png" class="img-fluid brasao-img" alt="Brasão da Prefeitura de Vargem Grande do Sul"></p>
+  <p class="brasao-titulo h5">Prefeitura Municipal de Vargem Grande do Sul</p>
+  <p class="brasao-cognome h4">"A Pérola da Mantiqueira"</p>
+
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home.php">Divisão de Informática</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="row align-items-end">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Licitações de TI
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="arps.php">ARPs</a></li>
+            <li><a class="dropdown-item" href="contratos.php">Contratos</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Legislação de TI
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="leis.php">Leis</a></li>
+            <li><a class="dropdown-item" href="decretos.php">Decretos</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Suporte
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="chamados.php">Chamados</a></li>
+            <li><a class="dropdown-item" href="downloads.php">Downloads</a></li>
+            <li><a class="dropdown-item" href="enderecos.php">Endereços Eletrônicos</a></li>
+            <li><a class="dropdown-item" href="avisos.php">Avisos</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Controles
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="insumos-impressao.php">Insumos de Impressão</a></li>
+            <li><a class="dropdown-item" href="laudos.php">Laudos</a></li>
+            <li><a class="dropdown-item" href="inventario.php">Inventário</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php">Contato</a>
+        </li>
+      </ul>
+      
+    </div>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
