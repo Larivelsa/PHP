ab369574390dc79152f4e2bd82d93744d5c853e6
Larissa <larissavelsa@gmail.com>
2021-11-08 11:11:35 -0300

    alterações no head (favicon e title)

--- projetos/ti-vgsul/pages/chamados.php
+++ projetos/ti-vgsul/pages/chamados.php
@@ -7,6 +7,8 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <meta name="viewport" content="width=device-width, charset=utf-8 initial-scale=1" http-equiv="Content-Type">
+      <link rel="icon" href="../static/img/favicon.ico" type="image/x-icon"/>
+      <title>Chamados - TI Prefeitura de Vargem Grande do Sul</title>
   </head>
   <body>
     <?php include "nav.php"; ?>
@@ -15,5 +17,6 @@
       <h6>Abertura de chamados</h6>
       <i class="fa fa-paper-plane-o" aria-hidden="true"></i> <a href="https://docs.google.com/forms/d/e/1FAIpQLSfJQzymHsBpG5sYOQDT_bawFa0yv7zMR1SpPB8NkHHGO_t0PQ/viewform">Reclamação de Insumos de Impressão adquiridos por processo licitatório</a>
     </div>
+    <?php include 'footer.php';?>
 </body>
 </html>
--- projetos/ti-vgsul/pages/licitacoes.php
+++ projetos/ti-vgsul/pages/licitacoes.php
@@ -6,12 +6,15 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <meta name="viewport" content="width=device-width, charset=utf-8 initial-scale=1" http-equiv="Content-Type">
+      <link rel="icon" href="../static/img/favicon.ico" type="image/x-icon"/>
+      <title>Licitações - TI Prefeitura de Vargem Grande do Sul</title>
   </head>
   <body>
     <?php include "nav.php";?>
 
 
   </body>
+  <?php include 'footer.php';?>
 </html>
 
 
